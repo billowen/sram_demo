8cbd3d00614b1564583c2f2bf2f24baecca57e96
KP Shao <shaokangpeng@semitronix.com>
2018-02-23 17:28:25 +0800

    create project

---
+++ src/main/java/com/example/sramdisplay/SramdisplayApplication.java
@@ -0,0 +1,12 @@
+package com.example.sramdisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SramdisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SramdisplayApplication.class, args);
+	}
+}
